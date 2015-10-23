7f25c381b74c3975f533b8b318553477b3a56cfd
Taylor Raboin <hello@tay.is>
2015-10-23 15:19:16 -0500

    added basic schools collection and pub/subs to listings page

--- client/templates/map/school_listings.js
+++ client/templates/map/school_listings.js
@@ -1,17 +1,5 @@
-var schoolsData = [
-	{
-		name: 'Hume-Fogg High School',
-		type: 'Magnet'
-	},
-	{
-		name: 'University School of Nashville',
-		type: 'Non-secular Private'
-	},
-	{
-		name: 'Nashville Big Picture High School',
-		type: 'Public Neighborhood'
-	}
-];
 Template.schoolListings.helpers({
-	schools: schoolsData
+	schools: function() {
+		return Schools.find();
+	}
 });
