b00fc5ada9264c281328f084539d350a37b8d838
Taylor Raboin <hello@tay.is>
2015-10-26 02:44:04 -0500

    changed ID routes to work after mongoimport, commented fixtures

--- server/fixtures.js
+++ server/fixtures.js
@@ -1,16 +1,16 @@
-if (Schools.find().count() === 0) {
-  Schools.insert({
-    name: 'Hume-Fogg High School',
-    type: 'Magnet'
-  });
+// if (Schools.find().count() === 0) {
+//   Schools.insert({
+//     name: 'Hume-Fogg High School',
+//     type: 'Magnet'
+//   });
 
-  Schools.insert({
-    name: 'University School of Nashville',
-    type: 'Non-secular Private'
-  });
+//   Schools.insert({
+//     name: 'University School of Nashville',
+//     type: 'Non-secular Private'
+//   });
 
-  Schools.insert({
-    name: 'Nashville Big Picture High School',
-    type: 'Public Neighborhood'
-  });
-}
+//   Schools.insert({
+//     name: 'Nashville Big Picture High School',
+//     type: 'Public Neighborhood'
+//   });
+// }
