02b5712d530a11d391992b55fa887407f37c41f4
Taylor Raboin <hello@tay.is>
2015-11-19 17:51:46 -0600

    removed external json api, started filter/populate map script

--- client/templates/map/school_map.js
+++ client/templates/map/school_map.js
@@ -2,6 +2,42 @@ Template.schoolMap.onRendered(function() {
   GoogleMaps.load();
 });
 
+Template.schoolMap.onCreated(function() {
+
+  // use the 'ready' callback to interact with the API once the map is ready
+  GoogleMaps.ready('map', function(map) {
+
+		// listen for when the map is finished being panned or zoomed
+		new google.maps.event.addListener( map.instance , 'idle' , function(){
+
+      // get the coordinate bounds of the map
+			var nelat = GoogleMaps.maps.map.instance.getBounds().getNorthEast().lat();
+      var nelng = GoogleMaps.maps.map.instance.getBounds().getNorthEast().lng();
+      var swlat = GoogleMaps.maps.map.instance.getBounds().getSouthWest().lat();
+      var swlng = GoogleMaps.maps.map.instance.getBounds().getSouthWest().lng();
+
+      // get all the schools in the database
+      var schools = Schools.find().fetch();
+
+      // iterate through each school, only showing the schools that are within the current bounds
+      schools.forEach( function(school){
+        if (school.coordinates.lat < nelat && school.coordinates.lat > swlat && school.coordinates.lng < nelng && school.coordinates.lng > swlng) {
+          
+          // test if results are limited by bounds
+          console.log(school.name);
+
+          // test adding school marker to the map
+          var marker = new google.maps.Marker({
+            position: school.coordinates,
+            map: map.instance
+          });
+        }
+
+      });
+		});
+  });
+});
+
 Template.schoolMap.helpers({
   mapOptions: function() {
     // make sure the API has loaded
@@ -14,33 +50,4 @@ Template.schoolMap.helpers({
       };
     }
   }
-});
-
-Template.schoolMap.onCreated(function() {
-
-  // use the 'ready' callback to interact with the API once the map is ready
-  GoogleMaps.ready('map', function(map) {
-
-  	// returns the NE/SW bounds of the map
-		function get_bounds(){
-			return {
-        nelat : GoogleMaps.maps.map.instance.getBounds().getNorthEast().lat(),
-        nelng : GoogleMaps.maps.map.instance.getBounds().getNorthEast().lng(),
-        swlat : GoogleMaps.maps.map.instance.getBounds().getSouthWest().lat(),
-        swlng : GoogleMaps.maps.map.instance.getBounds().getSouthWest().lng()
-			};
-		}
-
-		// call for bounds whenever the map is finished being panned or zoomed
-		new google.maps.event.addListener( map.instance , 'idle' , function(){
-			console.log( get_bounds() );
-		});
-
-    // template for adding a marker to the map
-    var marker = new google.maps.Marker({
-      position: map.options.center,
-      map: map.instance
-    });
-
-  });
 });
